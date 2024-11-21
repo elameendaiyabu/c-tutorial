21ddf898a84d3bb86c68c679a8f11ec47210373a
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-11-21 18:46:39 +0000

    initial commit

---
+++ user-information/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("hello my name is elameen\n");
+	printf("i am 20 years old\n");
+	printf("i like to watch crime tv shows when i am bored\n");
+	return 0;
+}
