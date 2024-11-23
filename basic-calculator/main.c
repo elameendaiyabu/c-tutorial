145d6a0278b0c84914ed35f3b9abbc5da50a1182
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-11-23 15:19:33 +0000

    basic calculator using if statement done

---
+++ basic-calculator/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+	int firstNum;
+	int secondNum;
+	char operation[1];
+	int result;
+	bool isRunning = true;
+
+	while (isRunning) {
+	
+		printf("Enter first number : ");
+		scanf("%d", &firstNum);
+
+		printf("Enter second number : ");
+		scanf("%d", &secondNum);
+
+		printf("choose operation (+, -, *, /, or q to quit) : ");
+		scanf("%s", operation);
+
+		if (strcmp(operation, "+") == 0){
+			result = firstNum + secondNum;
+		}else if (strcmp(operation, "-") == 0) {
+			result = firstNum - secondNum;
+		}else if (strcmp(operation, "*") == 0) {
+			result = firstNum * secondNum;
+		}else if (strcmp(operation, "/") == 0) {
+			result = firstNum / secondNum;
+		}else if (strcmp(operation, "q")== 0) {
+			break;
+		} else {
+			printf("Invalid character");
+		}
+
+		printf("%d\n",result);
+	}
+
+	return 0;
+}
